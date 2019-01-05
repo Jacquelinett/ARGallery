4ae61f0208ff5709d6ed67effbee2693637ee71b
Jacqueline Tran <jacqueline@Jacquelines-MacBook-Pro.local>
2019-01-05 14:51:09 -0500

    Finish up

---
+++ ARtGallery/Core/ARVideoNode.m
@@ -0,0 +1,12 @@
+//
+//  ARVideoNode.m
+//  ARtGallery
+//
+//  Created by Jacqueline Tran on 11/19/18.
+//
+
+#import "ARVideoNode.h"
+
+@implementation ARVideoNode
+
+@end
---
+++ ARtGallery/Miscanellous/AddModeEnum.h
@@ -0,0 +1,12 @@
+//
+//  AddModeEnum.h
+//  ARtGallery
+//
+//  Created by Jacqueline Tran on 11/19/18.
+//
+
+#ifndef AddModeEnum_h
+#define AddModeEnum_h
+
+
+#endif /* AddModeEnum_h */
